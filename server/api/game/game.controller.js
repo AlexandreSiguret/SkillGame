397f5bad85b23b6845f40567a20bdf2fcde906a2
DESKTOP-A2917M1\htsa <heditheo1@gmail.com>
2017-11-16 17:05:14 +0100

    fin du travail alexandre et moi bon week end

--- server/api/game/game.controller.js
+++ server/api/game/game.controller.js
@@ -11,11 +11,11 @@
 'use strict';
 
 import jsonpatch from 'fast-json-patch';
-import {Game} from '../../sqldb';
+import { Game } from '../../sqldb';
 import config from '../../config/environment';
 import Sequelize from 'sequelize';
-import {Question} from '../../sqldb';
-import {Answer} from "../../sqldb";
+import { Question } from '../../sqldb';
+import { Answer } from "../../sqldb";
 
 
 
@@ -23,8 +23,8 @@ const Op = Sequelize.Op;
 
 function respondWithResult(res, statusCode) {
   statusCode = statusCode || 200;
-  return function(entity) {
-    if(entity) {
+  return function (entity) {
+    if (entity) {
       return res.status(statusCode).json(entity);
     }
     return null;
@@ -32,11 +32,11 @@ function respondWithResult(res, statusCode) {
 }
 
 function patchUpdates(patches) {
-  return function(entity) {
+  return function (entity) {
     try {
       // eslint-disable-next-line prefer-reflect
       jsonpatch.apply(entity, patches, /*validate*/ true);
-    } catch(err) {
+    } catch (err) {
       return Promise.reject(err);
     }
 
@@ -45,8 +45,8 @@ function patchUpdates(patches) {
 }
 
 function removeEntity(res) {
-  return function(entity) {
-    if(entity) {
+  return function (entity) {
+    if (entity) {
       return entity.destroy()
         .then(() => {
           res.status(204).end();
@@ -56,8 +56,8 @@ function removeEntity(res) {
 }
 
 function handleEntityNotFound(res) {
-  return function(entity) {
-    if(!entity) {
+  return function (entity) {
+    if (!entity) {
       res.status(404).end();
       return null;
     }
@@ -67,7 +67,7 @@ function handleEntityNotFound(res) {
 
 function handleError(res, statusCode) {
   statusCode = statusCode || 500;
-  return function(err) {
+  return function (err) {
     res.status(statusCode).send(err);
   };
 }
@@ -79,32 +79,32 @@ export function index(req, res) {
     .catch(handleError(res));
 }
 
-export function freeGame(req,res){
-  
+export function freeGame(req, res) {
+
   return Game.findAll({
-    where :{
-      user1 : { $ne : req.user._id },
-      concept : req.params.id,
-      user2 : null
+    where: {
+      user1: { $ne: req.user._id },
+      concept: req.params.id,
+      user2: null
     },
-    order :         Sequelize.fn('RANDOM')     
-  }) .then(handleEntityNotFound(res))
-  .then(respondWithResult(res))
-  .catch(handleError(res));
+    order: Sequelize.fn('RANDOM')
+  }).then(handleEntityNotFound(res))
+    .then(respondWithResult(res))
+    .catch(handleError(res));
 }
 
 
-export function game(req,res){
+export function game(req, res) {
   var userId = req.user._id;
-  
+
   return Game.findAll({
-    
-    where :{
-      user1 : req.user._id
+
+    where: {
+      user1: req.user._id
     }
-  }) .then(handleEntityNotFound(res))
-  .then(respondWithResult(res))
-  .catch(handleError(res));
+  }).then(handleEntityNotFound(res))
+    .then(respondWithResult(res))
+    .catch(handleError(res));
 }
 
 // Gets a single Game from the DB
@@ -121,48 +121,48 @@ export function show(req, res) {
 
 
 // Creates a new Game in the DB
-function allcreation(tab,idquizz,iduser){
-var taille = tab.length;
+function allcreation(tab, idquizz, iduser) {
+  var taille = tab.length;
 
-for (var i = 0;i < taille;i++){
+  for (var i = 0; i < taille; i++) {
 
-var creation1 = {
-  question : tab[i].dataValues._id,
-  user : iduser,
-  quizz : idquizz
-}
-var creation2 = {
-  question : tab[i].dataValues._id,
-  quizz : idquizz
-}
+    var creation1 = {
+      question: tab[i].dataValues._id,
+      user: iduser,
+      quizz: idquizz
+    }
+    var creation2 = {
+      question: tab[i].dataValues._id,
+      quizz: idquizz
+    }
 
-Answer.create(creation1)
-Answer.create(creation2)
+    Answer.create(creation1)
+    Answer.create(creation2)
 
-}
+  }
 
 }
 
 export function create(req, res) {
-  req.body.user1= req.user._id
+  req.body.user1 = req.user._id
 
-  
+
   var new_game = {
-    user1 : 1,
-    concept : 2,
+    user1: 1,
+    concept: 2,
   }
-  
+
   return Game.create(req.body)
-  .then(response =>{
-    console.log("on essaye")
-    Question.findAll({ 
-      order :         Sequelize.fn( 'RANDOM' ) ,    
-      limit : 2
-    }).then(succes => allcreation(succes,response.dataValues._id,req.user._id))
-    .then(respondWithResult(res))
-    .catch(handleError(res));
-  })
-  
+    .then(response => {
+      console.log("on essaye")
+      Question.findAll({
+        order: Sequelize.fn('RANDOM'),
+        limit: 2
+      }).then(succes => allcreation(succes, response.dataValues._id, req.user._id))
+        .then(respondWithResult(res))
+        .catch(handleError(res));
+    })
+
   /*
   .then(response =>{
     console.log(response.dataValues);
@@ -176,40 +176,56 @@ export function create(req, res) {
     
     Answer.create(new_answer)}
   )   */ .then(respondWithResult(res, 201))
-/*
-
-  return Game.create(req.body)
-  .then(() =>{
-    Question.findAll({ 
-      order :         Sequelize.fn( 'RANDOM' ) ,    
-      limit : 1
-    }).then(res2 =>{     
-      var new_answer = {
-        question : res2[0].dataValues._id ,    
-        earnedPoint : 15,
-        user : req.body.user1,
-        quizz : req.body._id
-      }
-      Answer.create(new_answer)
-    })  
-
-  }   
+    /*
     
-  )    
-  .then(respondWithResult(res, 201))
-
- */
-  .catch(handleError(res));
+      return Game.create(req.body)
+      .then(() =>{
+        Question.findAll({ 
+          order :         Sequelize.fn( 'RANDOM' ) ,    
+          limit : 1
+        }).then(res2 =>{     
+          var new_answer = {
+            question : res2[0].dataValues._id ,    
+            earnedPoint : 15,
+            user : req.body.user1,
+            quizz : req.body._id
+          }
+          Answer.create(new_answer)
+        })  
+    
+      }   
+        
+      )    
+      .then(respondWithResult(res, 201))
+    
+     */
+    .catch(handleError(res));
 }
 
-function regularisation(idquizz,idplayer){
+function regularisation(idquizz,idplayer) {
   console.log("coucou toi")
-  return Answer.find({
+  console.log(idquizz)
+  return Answer.findAll({
     where: {
-      _id: idquizz,
-      user : null
+      quizz: idquizz,
+      user: null
     }
-  }).then(response =>console.log(response))
+  }).then(response => ajoutmultiple(response,idplayer))
+}
+
+function ajoutmultiple(tab,idplayer){
+  var taille = tab.length;
+  for (var i = 0; i < taille; i++){
+    var bla = {
+      user : idplayer
+    }
+    console.log(tab[i].dataValues._id)
+    Answer.update(bla,{
+      where : {
+        _id : tab[i].dataValues._id
+      }
+    })
+  }
 }
 
 // Upserts the given Game in the DB at the specified ID
@@ -230,7 +246,7 @@ export function upsert(req, res) {
 
 // Updates an existing Game in the DB
 export function patch(req, res) {
-  if(req.body._id) {
+  if (req.body._id) {
     Reflect.deleteProperty(req.body, '_id');
   }
   return Game.find({
