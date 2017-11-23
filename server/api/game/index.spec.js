2e0e6edb1df73bce9c3b8566f92700ff18896f48
sylmolanus <syllamohamedlamine17@gmail.com>
2017-11-23 02:44:16 +0100

    création apidoc_game

--- server/api/game/index.spec.js
+++ server/api/game/index.spec.js
@@ -36,6 +36,27 @@ describe('Game API Router:', function() {
     gameIndex.should.equal(routerStub);
   });
 
+  /**
+ * @api {get} /games  Read data of Game 
+ * 
+ * @apiName GetGame 
+ * @apiGroup Game 
+ * @apiPermission none
+ *
+ * @apiParam {Number} UserId ID of User.
+ * @apiParam {Number} ConceptId ID of Concept.
+ * @apiParam {Boolean} ended Ended of game.
+ * 
+ * 
+ * @apiError GameNotFound The <code>id</code> of the Game was not found.
+ * 
+ *  @apiErrorExample {json} Error-Response(exemple):
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "GameNotFound"
+ *     }
+ */
+
   describe('GET /api/games', function() {
     it('should route to game.controller.index', function() {
       routerStub.get
@@ -43,6 +64,28 @@ describe('Game API Router:', function() {
         .should.have.been.calledOnce;
     });
   });
+
+ /**
+ * @api {get} /games/:id Read data of Game by id
+ * 
+ * @apiName GetGame
+ * @apiGroup Game
+ * @apiPermission none
+ *
+ * @apiParam {Number} UserId ID of User.
+ * @apiParam {Number} ConceptId ID of Concept.
+ * @apiParam {Boolean} ended Ended of game.
+ * 
+ * 
+ * @apiError GameNotFound The <code>id</code> of the Game was not found.
+ * 
+ *  @apiErrorExample {json} Error-Response(exemple):
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "GameNotFound"
+ *     }
+ */
+
 
   describe('GET /api/games/:id', function() {
     it('should route to game.controller.show', function() {
@@ -52,6 +95,27 @@ describe('Game API Router:', function() {
     });
   });
 
+   /**
+ * @api {post} /games  Create a new Game 
+ * 
+ * @apiName PostGame
+ * @apiGroup Game
+ * @apiPermission none
+ *
+ * @apiParam {Number} UserId ID of User.
+ * @apiParam {Number} ConceptId ID of Concept.
+ * @apiParam {Boolean} ended Ended of game.
+ *
+ * 
+ * @apiError GameNotFound Minimum of 5 characters required.
+ * 
+ *  @apiErrorExample {json} Error-Response(exemple):
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "error": "GameNameTooShort"
+ *     }
+ */
+
   describe('POST /api/games', function() {
     it('should route to game.controller.create', function() {
       routerStub.post
@@ -59,6 +123,27 @@ describe('Game API Router:', function() {
         .should.have.been.calledOnce;
     });
   });
+
+/**
+ * @api {put} /games/:id  Change a Game 
+ * 
+ * @apiName PutGame 
+ * @apiGroup Game 
+ * @apiPermission none
+ *
+ * @apiParam {Number} UserId ID of User.
+ * @apiParam {Number} ConceptId ID of Concept.
+ * @apiParam {Boolean} ended Ended of game.
+ *
+ * 
+ * @apiError GameNotFound Minimum of 5 characters required.
+ * 
+ *  @apiErrorExample {json} Error-Response(exemple):
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "error": "GameNameTooShort"
+ *     }
+ */
 
   describe('PUT /api/games/:id', function() {
     it('should route to game.controller.upsert', function() {
@@ -68,6 +153,28 @@ describe('Game API Router:', function() {
     });
   });
 
+  
+  /**
+ * @api {patch} /games/:id  Change a Game 
+ * 
+ * @apiName PatchGame
+ * @apiGroup Game
+ * @apiPermission none
+ *
+ * @apiParam {Number} UserId ID of User.
+ * @apiParam {Number} ConceptId ID of Concept.
+ * @apiParam {Boolean} ended Ended of game.
+ *
+ * 
+ * @apiError GameNotFound Minimum of 5 characters required.
+ * 
+ *  @apiErrorExample {json} Error-Response(exemple):
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "error": "GameNameTooShort"
+ *     }
+ */
+
   describe('PATCH /api/games/:id', function() {
     it('should route to game.controller.patch', function() {
       routerStub.patch
@@ -75,6 +182,24 @@ describe('Game API Router:', function() {
         .should.have.been.calledOnce;
     });
   });
+
+  /**
+ * @api {delete} /games/:id  delete a Game 
+ * 
+ * @apiName DeleteGame
+ * @apiGroup Game
+ * @apiPermission none
+ *
+ *
+ * 
+ * @apiError GameNotFound The <code>id</code> of Game was not found.
+ * 
+ *  @apiErrorExample {json} Error-Response(exemple):
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "GameNotFound"
+ *     }
+ */
 
   describe('DELETE /api/games/:id', function() {
     it('should route to game.controller.destroy', function() {
