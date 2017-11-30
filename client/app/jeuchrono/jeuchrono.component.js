015c0e8bd93d3cfef038aa10357da6db28198705
sylmolanus <syllamohamedlamine17@gmail.com>
2017-11-30 22:36:01 +0100

    recup new version

--- client/app/jeuchrono/jeuchrono.component.js
+++ client/app/jeuchrono/jeuchrono.component.js
@@ -11,37 +11,218 @@ export class JeuchronoComponent {
   $timeout;
   socket;
   stopped;
+  num = 0;
 
 
 
   /*@ngInject*/
   constructor($http, $scope, socket, $timeout, $stateParams) {
-    this.$timeout = $timeout;
-    this.$scope = $scope;
+    this.$timeout=$timeout;
+    this.$scope=$scope;
     this.$stateParams = $stateParams;
     this.$http = $http;
     this.socket = socket;
     $scope.counter = 30;
     $scope.stopped = false;
-    var vm = this;
-    this.errormessage= ""
+    var vm = this;    
+    this.errormessage = ""
     this.questionChoices=[];
-    this.singleQuestion= [];
+    this.singleQuestion=[];
     this.idChoices = [];
 
-    this.$http.get('/api/answers/pickone/'+this.$stateParams.concepts)
-    .then(response =>{
+    this.$http.get('/api/answers/pickone/'+this.$stateParams.concept_id)
+    .then(response => {
       this.singleQuestion = response.data[0];
+      //console.log("ooooooooooooooooooooooooooo");
+      //console.log(this.singleQuestion)
       this.$http.get("/api/choices/question/"+this.singleQuestion.Question._id)
       .then(response => {
         this.questionChoices = response.data;
-        console.log("ooooooooooooooooooo");
-        console.log(response.data);
+        // console.log("ooooooooooooooooooooooooooo");
+        // console.log()
+      });
+
+      this.$http.get("/api/questions/"+this.singleQuestion.Question._id)
+      .then(response => {
+        this.detailedQuestion = response.data;
+
+          for (var i = 0; i < 4; i++) {
+            vm.idChoices[i]=vm.questionChoices[i]._id;
+
+            if (vm.questionChoices[i].statement == vm.detailedQuestion.goodAnswer)
+              vm.detailedQuestion._id = vm.questionChoices[i]._id;
+          }
+
       });
     });
 
+          
+
+
+    $scope.$on('$destroy', function() {
+      socket.unsyncUpdates('question');
+    });
+
+    $scope.onTimeout = function(){
+
+      if($scope.counter == 0) {
+
+        
+        
+        var variable = '#label-choices-'+vm.detailedQuestion._id;
+        var myEl = angular.element( document.querySelector( variable ) );
+        myEl.removeAttr('class');
+        myEl.attr('class',"false");
+
+      
+        
+        for (var i = 0; i < vm.idChoices.length; i++) {
+          var variable = '#choices-'+vm.idChoices[i];
+          var myEl = angular.element( document.querySelector( variable ) );
+          myEl.attr('disabled',"");
+        }
+
+        if(vm.num < 1)
+        {
+          var myEl = angular.element(document.querySelector('#next-question-button'));
+          myEl.removeAttr('disabled');
+
+          var myEl = angular.element(document.querySelector('#report-question-button'));
+          myEl.removeAttr('disabled');
+        }       
+
+        
+       
+        $scope.stopped=true;
+
+        vm.$http.put('/api/answers/'+ vm.singleQuestion._id,{
+          _id :vm.singleQuestion._id,
+          earnedPoint : 0
+        })
+      }
+
+      if($scope.stopped != true)
+        {$scope.counter--;}
+
+      $scope.minutes = Math.floor((($scope.counter / 60)));
+      $scope.seconds = Math.floor($scope.counter - ($scope.minutes * 60));
+      mytimeout = $timeout($scope.onTimeout,1000);
+    }
+    var mytimeout = $timeout($scope.onTimeout,1000);
   }
-}
+
+  Check_next() {
+    this.errormessage = "";
+    var myEl = angular.element(document.querySelector('#next-question-button'));
+    myEl.attr('disabled',"");
+
+    var myEl = angular.element(document.querySelector('#report-question-button'));
+    myEl.attr('disabled',"");  
+
+    
+    this.$http.get('/api/answers/pickone/'+this.$stateParams.concept_id)
+    .then(response => {
+      this.singleQuestion = response.data[0];
+                console.log(this.singleQuestion.Question._id)
+                this.$http.get("/api/choices/question/"+this.singleQuestion.Question._id)
+                .then(response => {
+                  this.questionChoices = response.data;
+              });
+                this.$http.get("/api/questions/"+this.singleQuestion.Question._id)
+                .then(response => {
+                  this.detailedQuestion = response.data;
+
+                  for (var i = 0; i < 4; i++) {
+                    this.idChoices[i]=this.questionChoices[i]._id;
+
+                    if (this.questionChoices[i].statement == this.detailedQuestion.goodAnswer)
+                      this.detailedQuestion._id = this.questionChoices[i]._id;
+                  }
+                      });
+
+              });
+
+    this.num++;
+    this.$scope.counter = 30;
+    this.$scope.stopped=false;
+
+  }
+
+  report(){
+          // alert('Question Reported !');
+          if(this.errormessage ==""){
+
+            this.errormessage = "the question :"+this.singleQuestion
+            /*this.errormessage = "the question has been reported"*/
+            this.$http.get("/api/questions/"+this.awesomeQuestion[this.num]._id).then(response =>{
+              this.$http.put("/api/questions/"+this.awesomeQuestion[this.num]._id,{ 
+                _id : this.awesomeQuestion[this.num]._id,
+                nbContestation : response.data.nbContestation + 1
+              })
+            })
+          }
+        }
+
+        
+
+        validation(select){
+
+          if ( this.detailedQuestion.goodAnswer == select.statement ) {
+
+            this.$http.put('/api/answers/'+ this.singleQuestion._id,{
+              _id :this.singleQuestion._id,
+              earnedPoint : this.$scope.seconds
+            })
+
+            var variable = '#label-choices-'+select._id;
+            var myEl = angular.element( document.querySelector( variable ) );
+            myEl.removeAttr('class');
+            myEl.attr('class',"true");
+
+            for (var i = 0; i < this.idChoices.length; i++) {
+              var variable = '#choices-'+this.idChoices[i];
+              var myEl = angular.element( document.querySelector( variable ) );
+              myEl.attr('disabled',"");
+            }
+          }
+          else {
+           
+
+            this.$http.put('/api/answers/'+ this.singleQuestion._id,{
+              _id :this.singleQuestion._id,
+              earnedPoint : 0
+            })
+
+            var variable = '#label-choices-'+this.detailedQuestion._id;
+            var myEl = angular.element( document.querySelector( variable ) );
+            myEl.removeAttr('class');
+            myEl.attr('class',"true");
+
+            var variable2 = '#label-choices-'+select._id;
+            var myE2 = angular.element( document.querySelector( variable2 ) );
+            myE2.removeAttr('class');
+            myE2.attr('class',"false");
+
+            for (var i = 0; i < this.idChoices.length; i++) {
+              var variable = '#choices-'+this.idChoices[i];
+              var myEl = angular.element( document.querySelector( variable ) );
+              myEl.attr('disabled',"");
+            } 
+
+          }
+
+          if(this.num < 1) {
+            var myEl = angular.element(document.querySelector('#next-question-button'));
+            myEl.removeAttr('disabled');
+            var myEl = angular.element(document.querySelector('#report-question-button'));
+            myEl.removeAttr('disabled');
+          }
+          else {
+            this.$timeout(function() { alert('Test Terminer !! Redirection vers la page ..... !!');}, 2000);
+          }
+          this.$scope.stopped=true;
+        }
+      }
 
 export default angular.module('skillGameApp.jeuchrono', [uiRouter])
   .config(routes)
