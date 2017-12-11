873578c05895214c729c563e64cbf6f486fc156f
DESKTOP-A2917M1\htsa <heditheo1@gmail.com>
2017-12-11 10:07:29 +0100

    correction mineur pour revenir version precedente

--- client/app/questions/questions.component.js
+++ client/app/questions/questions.component.js
@@ -47,33 +47,11 @@ export class QuestionsController {
         for (var i = 0; i < this.awesomeConcept.length; i++) {
           this.allConceptId[i]=this.awesomeConcept[i]._id
         }
+        
         console.log(this.allConceptId)
       });
   }
 
-
-  /*uploadPic(file) {
-    file.upload = Upload.upload({
-      url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
-      data: {
-        username: $scope.username,
-        file: file
-      },
-    });
-
-    file.upload.then(function(response) {
-      $timeout(function() {
-        file.result = response.data;
-      });
-    }, function(response) {
-      if (response.status > 0)
-        $scope.errorMsg = response.status + ': ' + response.data;
-    }, function(evt) {
-      // Math.min is to fix IE which reports 200% sometimes
-      file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-    });
-  }
-*/
 
 
   choix_concept(concept) {
