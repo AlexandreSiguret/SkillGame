8430ee00666883bb3a74853e4bdc6ca038ac8ca0
DESKTOP-A2917M1\htsa <heditheo1@gmail.com>
2017-12-11 09:30:32 +0100

    legere correction

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -11,9 +11,9 @@ router.get('/', auth.isAuthenticated(), controller.index);
 router.get("/notme",auth.isAuthenticated(),controller.notme);
 router.delete('/:id', auth.hasRole('admin'), controller.destroy);
 router.get('/me', auth.isAuthenticated(), controller.me);
-router.put('/upload', auth.isAuthenticated(), controller.uploadFiles);
 router.put('/:id/password', auth.isAuthenticated(), controller.changePassword);
 router.get('/:id', auth.isAuthenticated(), controller.show);
+router.post('/upload', auth.isAuthenticated(), controller.uploadFiles);
 router.post('/', controller.create);
 
 module.exports = router;
